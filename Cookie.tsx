ea680697eff7aa2cc8c593a1d1aed38a8f25256f
chopkarmanoj <54398998+chopkarmanoj@users.noreply.github.com>
2023-08-11 13:36:35 +0000

    Add files via upload

---
+++ Cookie.tsx
@@ -0,0 +1,99 @@
+interface CookieConfig {
+  name: string;
+  label: string;
+  description: string;
+  cookies: string[];
+  onAccept: () => void;
+  onRevoke: () => void;
+}
+
+interface Config {
+  apiKey: string;
+  product: string;
+  optionalCookies: CookieConfig[];
+  position: 'LEFT' | 'RIGHT';
+  theme: 'LIGHT' | 'DARK';
+}
+
+declare global {
+  interface Window {
+    CookieControl: {
+      load: (config: Config) => void;
+    };
+  }
+}
+
+const config: Config = {
+  apiKey: '3a0578b34b8e13d5aa9fb91166af07c105fb2e0d',
+  product: 'community',
+  optionalCookies: [
+    {
+      name: 'analytics',
+      label: 'Analytics',
+      description: '',
+      cookies: [],
+      onAccept: () => {},
+      onRevoke: () => {},
+    },
+    {
+      name: 'marketing',
+      label: 'Marketing',
+      description: '',
+      cookies: [],
+      onAccept: () => {},
+      onRevoke: () => {},
+    },
+    {
+      name: 'preferences',
+      label: 'Preferences',
+      description: '',
+      cookies: [],
+      onAccept: () => {},
+      onRevoke: () => {},
+    },
+  ],
+  position: 'RIGHT',
+  theme: 'DARK',
+};
+
+if (typeof window !== 'undefined') {
+  (window as Window).CookieControl?.load(config);
+}
+const Cookie = () => {
+  const config: Config = {
+    apiKey: '3a0578b34b8e13d5aa9fb91166af07c105fb2e0d',
+    product: 'community',
+    optionalCookies: [
+      {
+        name: 'analytics',
+        label: 'Analytics',
+        description: '',
+        cookies: [],
+        onAccept: () => {},
+        onRevoke: () => {},
+      },
+      {
+        name: 'marketing',
+        label: 'Marketing',
+        description: '',
+        cookies: [],
+        onAccept: () => {},
+        onRevoke: () => {},
+      },
+      {
+        name: 'preferences',
+        label: 'Preferences',
+        description: '',
+        cookies: [],
+        onAccept: () => {},
+        onRevoke: () => {},
+      },
+    ],
+    position: 'RIGHT',
+    theme: 'DARK',
+  };
+
+  return <>{typeof window !== 'undefined' && (window as Window).CookieControl?.load(config)}</>;
+};
+
+export default Cookie;
